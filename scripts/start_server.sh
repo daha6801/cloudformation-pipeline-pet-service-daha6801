03c233c8492e385b3a50408d27e43a5a7cb0bf38
Rashmi Dahal <daha6801@stthomas.edu>
2022-07-08 12:21:11 -0500

    fix for node doesn't exist problem

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
+node -e "console.log('Running Node.js ' + process.version)"
+node --version
 node pet-services/app.js
 
