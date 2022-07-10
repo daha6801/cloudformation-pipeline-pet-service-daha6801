3657ce6a408aa3cd4b6082f4f0a0a99fee67c156
Rashmi Dahal <daha6801@stthomas.edu>
2022-07-10 09:07:32 -0500

    move install to start_server again -

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -15,7 +15,7 @@ sudo npm i -g pm2
 pm2 start app.js
 #sudo apt update
 sudo apt-get update && sudo apt-get -y upgrade
-sudo apt install nginx
+sudo apt install -y nginx
 export PATH=$PATH:/usr/local/nginx/
 nginx -v
 sudo cp /scripts/nginx-default-replace /etc/nginx/sites-enabled/default
