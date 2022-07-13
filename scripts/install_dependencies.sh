17fb2bacdb5ee17e712dee4d8f8eef145fbc0329
Rashmi Dahal <daha6801@stthomas.edu>
2022-07-13 11:49:07 -0500

    upd

--- scripts/install_dependencies.sh
+++ scripts/install_dependencies.sh
@@ -1,14 +1,20 @@
 #!/bin/bash
+
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
 . ~/.nvm/nvm.sh
 export NVM_DIR="$HOME/.nvm"
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
 nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
+sudo apt install npm
 node --version
+
 npm install express
 npm install pm2 -g
-nginx -v
+
 sudo apt-get update && sudo apt-get -y upgrade
 sudo apt-get install -y nginx
 export PATH=$PATH:/usr/local/nginx/
+nginx -v
+
+exit
--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,18 +1,14 @@
 #!/bin/bash
-#curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
-#. ~/.nvm/nvm.sh
-#export NVM_DIR="$HOME/.nvm"
-#[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
-#nvm install --lts
+
 node -e "console.log('Running Node.js ' + process.version)"
 node --version
 pwd
-cd /pet-services # Go to tge project directory
+cd /pet-services # Go to the project directory
 
 sudo npm i -g pm2
 pm2 start app.js
-nginx -v
 
+nginx -v
 sudo cp /scripts/nginx-default-replace /etc/nginx/sites-enabled/default
 sudo service nginx restart
 exit
