6483f05f3e22f31ad8b1dcbf34694e267844311b
Rashmi Dahal <daha6801@stthomas.edu>
2022-07-28 08:21:38 -0500

    update

--- scripts/start_server.sh
+++ scripts/start_server.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.34.0/install.sh | bash
+. ~/.nvm/nvm.sh
+export NVM_DIR="$HOME/.nvm"
+[ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh" # This loads nvm
+nvm install --lts
 node -e "console.log('Running Node.js ' + process.version)"
+sudo apt install npm
 node --version
+
+npm install express
+npm install pm2 -g
+
 pwd
 cd /pet-services # Go to the project directory
 
